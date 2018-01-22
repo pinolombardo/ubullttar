9457d278f10be932fa2d378a5591473750fbb516
Pino <giuseppe.lombardo88@gmail.com>
2018-01-22 11:29:29 +0100

    aggiorna rest

--- src/providers/rest/rest.ts
+++ src/providers/rest/rest.ts
@@ -11,49 +11,49 @@ export class RestProvider {
   }
 
   getData() {
-    var url = 'https://www.ubullttar.it/?json=get_posts&count=20';
+    var url = 'http://www.ubullttar.it/?json=get_posts&count=20';
     var response = this.http.get(url).map(res => res.json());
     console.log(response);
     return response;    
       }     
 
   getDataProno() {
-     var url = 'https://www.ubullttar.it/?json=get_category_posts&category_id=31&count=20';
+     var url = 'http://www.ubullttar.it/?json=get_category_posts&category_id=31&count=20';
      var response = this.http.get(url).map(res => res.json());
      console.log(response);
      return response;   
       }
             
    getDataMultiple() {
-        var url = 'https://www.ubullttar.it/?json=get_category_posts&category_id=32&count=20';
+        var url = 'http://www.ubullttar.it/?json=get_category_posts&category_id=32&count=20';
         var response = this.http.get(url).map(res => res.json());
         console.log(response);
         return response;   
       }
 
     getDataBasket() {
-        var url = 'https://www.ubullttar.it/?json=get_category_posts&category_id=36&count=20';
+        var url = 'http://www.ubullttar.it/?json=get_category_posts&category_id=36&count=20';
         var response = this.http.get(url).map(res => res.json());
         console.log(response);
         return response;   
       }
       
       getDataProgressione() {
-        var url = 'https://www.ubullttar.it/?json=get_category_posts&category_id=405&count=20';
+        var url = 'http://www.ubullttar.it/?json=get_category_posts&category_id=405&count=20';
         var response = this.http.get(url).map(res => res.json());
         console.log(response);
         return response;
       }
 
       getDataVincite() {
-        var url = 'https://www.ubullttar.it/?json=get_page&page_id=1815'; 
+        var url = 'http://www.ubullttar.it/?json=get_page&page_id=1815'; 
         var response = this.http.get(url).map(res => res.json());
         console.log(response);
         return response;
       }
 
       getDataBonus() {
-        var url = 'https://www.ubullttar.it/?json=get_page&page_id=1815';
+        var url = 'http://www.ubullttar.it/?json=get_page&page_id=1815';
         var response = this.http.get(url).map(res => res.json());
         console.log(response);
         return response; 
